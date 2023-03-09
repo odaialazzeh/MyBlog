ec34542e7201b2af62673ea4a34af73a8ac3f9f0
odaialazzeh <odai.alazzeh@gmail.com>
2023-03-09 17:11:36 +0200

    Add validations to models

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,12 @@
 class User < ApplicationRecord
-  has_many :posts, inverse_of: :author
-  has_many :comments, inverse_of: :author
-  has_many :likes, inverse_of: :author
+  has_many :posts, inverse_of: :author, foreign_key: :author_id
+  has_many :comments, inverse_of: :author, foreign_key: :author_id
+  has_many :likes, inverse_of: :author, foreign_key: :author_id
 
-  def recent_posts
-    posts.order(created_at: :desc).limit(3)
+  validates :name, presence: true
+  validates :post_counter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
+
+  def recent_posts(limit = 3)
+    posts.order(created_at: :desc).limit(limit)
   end
 end
